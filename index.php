f64c4fad58d376963d5e34a462ade4b4c5acee3f
sona3108 <149369726+sona3108@users.noreply.github.com>
2023-10-30 14:57:31 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+   include("header.php");
+?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.bg-image {
+  /* The image used */
+  background-image: url("images/2.jpg");
+  
+  /* Add the blur effect */
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+  
+  /* Full height */
+  height: 100%; 
+  
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+/* Position text in the middle of the page/image */
+.bg-text {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 80%;
+  padding: 20px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="bg-image"></div>
+
+<div class="bg-text">
+ 
+  <h1 style="font-size:100px">BLOOD BANK HOSPITAL</h1>
+  <p><h4> “Donate blood, be a lifesaver.”</h4></p>
+</div>
+
+
+
+<?php
+   include("footer.php");
+?>
+
+
